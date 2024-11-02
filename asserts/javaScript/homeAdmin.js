21b2c2630060deeae3fdb764c480836eab6704e4
MaiTram01 <tramh7879@gmail.com>
2024-11-02 19:46:51 +0700

    Done/Admin

---
+++ asserts/javaScript/homeAdmin.js
@@ -0,0 +1,531 @@
+const cardList = [
+    
+    {
+        id: 1,
+        image: "../Homepage/Image/sp1.jpg",
+        name: "MISS DIOR",
+        title: "Eau de parfum - floral and fresh notes",
+        price: 125.00,
+        gender: "Man",
+        type: "perfume",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát và quyến rũ...",
+        capacity: "50 ml",
+        color: "pink",
+        category: "MISS DIOR"
+    },
+    {
+        id: 2,
+        image: "../Homepage/Image/sp2.jpg",
+        name: "MISS DIOR BLOOMING BOUQUET",
+        title: "Eau de toilette",        
+        price: 164.00,
+        gender: "Man",
+        type: "perfume",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát...",
+        capacity: "100 ml",
+        color: "yellow",
+        category: "MISS DIOR BLOOMING BOUQUET"
+    },
+    {
+        id: 3,
+        image: "../Homepage/Image/sp3.jpg",
+        name: "MISS DIOR ABSOLUTELY BLOOMING",
+        title: "Eau de parfum",
+        price: 125.00,
+        gender: "Man",
+        type: "perfume",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát...",
+        capacity: "300 ml",
+        color: "pink",
+        category: "MISS DIOR ABSOLUTELY BLOOMING"
+    },
+    {
+        id: 4,
+        image: "../Homepage/Image/sp10.jpg",
+        name: "SAUVAGE",
+        title: "makeup",
+        price: 145.00,
+        gender: "Man",
+        type: "makeup",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát...",
+        capacity: "300 ml",
+        color: "blue",
+        category: "SAUVAGE"
+    },
+    {
+        id: 5,
+        image: "../Homepage/Image/sp11.jpg",
+        name: "SAUVAGE",
+        title: "Elixir",
+        price: 230.00,
+        gender: "Man",
+        type: "makeup",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát...",
+        capacity: "350 ml",
+        color: "black",
+        category: "SAUVAGE"
+    },
+    {
+        id: 6,
+        image: "../Homepage/Image/sp12.jpg",
+        name: "SAUVAGE",
+        title: "Eau de toilette",
+        price: 107.00,
+        gender: "Man",
+        type: "skincare",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát...",
+        capacity: "250 ml",
+        color: "Silver",
+        category: "SAUVAGE"
+    },
+    {
+        id: 7,
+        image: "../Homepage/Image/sp13.jpg",
+        name: "ROUGE DIOR FOREVER LIPSTICK",
+        title: "Transfer-proof lipstick pigmented matte-bare-lip feel comfort",
+        price: 66.00,
+        gender: "Woman",
+        type: "skincare",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát...",
+        capacity: "100 ml",
+        color: "Red",
+        category: "ROUGE DIOR FOREVER LIPSTICK"
+    },
+    {
+        id: 8,
+        image: "../Homepage/Image/sp14.jpg",
+        name: "ROUGE DIOR FOREVER LIQUID",
+        title: "Transfer-proof liquid lipstick - ultra-pigmented matte- weightless comfort",
+        price: 64.00,
+        gender: "Woman",
+        type: "skincare",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát...",
+        capacity: "200 ml",
+        color: "pink",
+        category: "ROUGE DIOR FOREVER LIQUID"
+    },
+    {
+        id: 9,
+        image: "../Homepage/Image/sp15.jpg",
+        name: "DIOR FOREVER SKIN GLOW",
+        title: "Clean Radiant Foundation - 24 Wear and Hydration",
+        price: 99.00,
+        gender: "Woman",
+        type: "clothing",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát...",
+        capacity: "150 ml",
+        color: "Beige",
+        category: "DIOR FOREVER SKIN GLOW"
+    },
+    {
+        id: 10,
+        image: "../Homepage/Image/sp16.jpg",
+        name: "DIOR PRESTIGE LE BAUME DE MINUIT",
+        title: "Intensive Revitalizing Night Cream",
+        price: 895.00,
+        gender: "Woman",
+        type: "clothing",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát...",
+        capacity: "350 ml",
+        color: "Gold",
+        category: "DIOR PRESTIGE LE BAUME DE MINUIT"
+    },
+    {
+        id: 11,
+        image: "../Homepage/Image/sp17.jpg",
+        name: "DIOR PRESTIGE",
+        title: "La Micro-Huile de Rose Advanced Serum",
+        price: 238.00,
+        gender: "Woman",
+        type: "clothing",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát...",
+        capacity: "50 ml",
+        color: "pink",
+        category: "DIOR PRESTIGE"
+    },
+    {
+        id: 12,
+        image: "../Homepage/Image/sp18.jpg",
+        name: "DIOR PRESTIGE LA CRÈME",
+        title: "Anti-Aging Intensive Repairing Cream",
+        price: 569.00,
+        gender: "Woman",
+        type: "accessories",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát...",
+        capacity: "100 ml",
+        color: "yellow",
+        category: "DIOR PRESTIGE LA CRÈME"
+    },
+    {
+        id: 13,
+        image: "../Homepage/Image/sp19.jpg",
+        name: "GRIS DIOR",
+        title: "Unisex eau de parfum - chypre notes",
+        price: 235.00,
+        gender: "Woman",
+        type: "accessories",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát...",
+        capacity: "200 ml",
+        color: "Gray",
+        category: "GRIS DIOR"
+    },
+    {
+        id: 14,
+        image: "../Homepage/Image/sp20.jpg",
+        name: "DIORIVIER",
+        title: "Eau de Parfum - Fig and Rose Notes",
+        price: 235.00,
+        gender: "Woman",
+        type: "accessories",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát...",
+        capacity: "100 ml",
+        color: "Green",
+        category: "DIORIVIER"
+    },
+    {
+        id: 15,
+        image: "../Homepage/Image/sp21.jpg",
+        name: "Dior Sauvage",
+        title: "Eau de Toilette",
+        price: 230.00,
+        gender: "Man",
+        type: "accessories",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát...",
+        capacity: "300 ml",
+        color: "blue",
+        category: "Dior Sauvage"
+    },
+    {
+        id: 16,
+        image: "../Homepage/Image/sp5.jpg",
+        name: "MISS DIOR BLOOMING BOUQUET",
+        title: "Collector's Bottle",
+        price: 140.00,
+        gender: "Woman",
+        type: "footwear",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát...",
+        capacity: "200 ml",
+        color: "blue",
+        category: "MISS DIOR BLOOMING BOUQUET"
+    },
+    {
+        id: 17,
+        image: "../Homepage/Image/sp4.jpg",
+        name: "LES ADORABLES SET",
+        title: "Shimmering Scurb, Body Cream and Shimmering Gel",
+        price: 437.00,
+        gender: "Woman",
+        type: "footwear",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát...",
+        capacity: "300 ml",
+        color: "black",
+        category: "LES ADORABLES SET"
+    },
+    {
+        id: 18,
+        image: "../Homepage/Image/sp6.jpg",
+        name: "DIOR LIP GLOW",
+        title: "Color Awakening Lip Balm",
+        price: 38.00,
+        gender: "Woman",
+        type: "footwear",
+        description: "Nước hoa Miss Dior Eau de Parfum là sự hòa quyện tinh tế giữa các tầng hương tươi mát...",
+        capacity: "250 ml",
+        color: "black",
+        category: "DIOR LIP GLOW"
+    }
+
+];
+// localStorage.setItem("products", JSON.stringify(products));
+if (!localStorage.getItem("cardList")) {
+localStorage.setItem("cardList", JSON.stringify(cardList));
+}
+// hamf hien thi so luon sp
+function showQuantity(){
+var quantities = JSON.parse(localStorage.getItem('cardList'));
+var qtyLength = quantities.length;
+document.getElementById('qty').innerHTML = 'Quantities: '+ qtyLength;
+}
+
+// document.addEventListener('click', function(event) {
+//     // Kiểm tra nếu click không nằm trong ul và ô input
+// if (!results.contains(event.target) && event.target.id !== 'update_pro') {
+//     results.style.display = 'none';
+// }
+// });
+showQuantity()
+// function thêm sản phẩm
+function addProduct() {
+var name = document.getElementById('name').value;
+var title = document.getElementById('title').value;
+var price = document.getElementById('price').value;
+var description = document.getElementById('comments').value;
+var category = document.getElementById('category').value;
+
+if (!name || !title || !price || !description || !category) {
+    alert("Vui lòng điền tất cả các trường!");
+    return;
+}
+
+// Xử lý hình ảnh
+var fileInput = document.getElementById('file-input');
+var image;
+if (fileInput.files.length > 0) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        image = e.target.result;
+        // Thêm sản phẩm sau khi đọc xong hình ảnh
+        saveProduct(name, title, price, description, image, category);
+        
+    };
+    reader.readAsDataURL(fileInput.files[0]);
+    alert('Success')
+    document.getElementById('productsModal').style.display = 'none';
+} else {
+    alert("Vui lòng chọn một hình ảnh!");
+}
+}
+// lưu thông tin
+function saveProduct(name, title, price, description, image, category) {
+var products = JSON.parse(localStorage.getItem("cardList"));
+var newProduct = {
+    id: products.length + 1,
+    name: name,
+    title: title,
+    price: parseFloat(price),
+    description: description,
+    image: image,
+    tp: document.querySelector('input[name="product"]:checked').value,
+    gender: document.querySelector('input[name="gender"]:checked').value,
+    Capacity: document.querySelector('input[name="capacity"]:checked').value,
+    color: document.querySelector('input[name="color"]:checked').value,
+    category: category
+};
+products.push(newProduct);
+localStorage.setItem("cardList", JSON.stringify(products));
+
+location.reload()
+
+
+}
+
+// Mở thêm sanr phẩm
+function openAddProduct() {
+document.getElementById('productsModal').style.display = 'block';
+}
+// Dong them san pham
+function closeAddproduct(){
+document.getElementById('productsModal').style.display = 'none';
+}
+// Hiển thị sản phẩm
+function showProducts() {
+var products = JSON.parse(localStorage.getItem("cardList"));
+
+if (products && products.length > 0) {
+    var tableContent = '';  
+
+    for (var i of products) {
+        var row = "<tr  style = 'border-bottom: 1px solid black; background-color: #faeef0;'>";
+        row += "<td style='padding-left: 124px;font-family: 'Kaisei Tokumin';'>" + i.id + "</td>";
+        row += "<td style='padding-left: 104px;font-family: 'Kaisei Tokumin';'>" + i.name + "</td>";
+        row += "<td style='padding-left: 96px;font-family: 'Kaisei Tokumin';'><img src='" + i.image + "' alt='Product Image' width='100' height='100;'></td>";
+        row += "<td style='padding-left: 120px;'> $" + i.price + "</td>";
+        row += "<td style='padding-left: 92px; font-family: Kaisei Tokumin; '>" +
+        "<button id = 'btn-edit' class='btn btn-success' onclick='updateData(\"" + i.id + "\")'>Edit</button> " +
+        "<button style='font-family: Kaisei Tokumin;' id = 'btn-delete' class='btn btn-danger' onclick='openFormRemove(\"" + i.id + "\")'>Delete</button></td>";
+        row += "</tr>";
+        tableContent += row; 
+    }
+    document.getElementById('content_table').innerHTML = tableContent;
+} else {
+    console.log("No products found in localStorage.");
+}
+}
+showProducts();
+// hamf mo form xoa SP
+var id_item;
+function openFormRemove(id){
+document.getElementById('formRemove').style.display = 'block';
+id_item = id;
+}
+function closeFormRemove(){
+document.getElementById('formRemove').style.display = 'none';
+}
+// Hàm mở form xac nhạn update
+function openFormUpdate(){
+document.getElementById('formUpdate').style.display = 'block';
+id_item = id;
+}
+function closeFormUpdate(){
+document.getElementById('formUpdate').style.display = 'none';
+document.getElementById('update_pro').style.display = 'none';
+
+
+}
+
+
+// Hàm xóa sản phẩm
+function removeData(){
+var products = JSON.parse(localStorage.getItem("cardList"));
+
+const newProducts = products.filter(function(product){
+    return String(product.id) !== String(id_item);
+});
+localStorage.setItem("cardList", JSON.stringify(newProducts));
+console.log(products)
+location.reload()
+
+}
+
+// Hiển thị dử liệu cần up date
+var idUpdate;
+function updateData(id) {
+idUpdate = id;
+document.getElementById('update_pro').style.display = 'block';
+var products = JSON.parse(localStorage.getItem("cardList"));
+if (!products) {
+    console.error("No products found in localStorage.");
+    return;
+}
+var product = products.find(function(product) {
+    return String(product.id) === String(id);
+});
+if (!product) {
+    console.error("Product not found with id:", id);
+    return;
+}
+document.getElementById("name_update").value = product.name || '';							
+document.getElementById("title_update").value = product.content || '';							
+document.getElementById("price_update").value = product.price || '';	
+document.getElementById("comments_update").value = product.description || '';	
+document.getElementById("image").value = product.image || '';	
+
+const radioButton = document.querySelector(`input[name="product_update"][value="${product.tp}"]`);
+if (radioButton) {
+    radioButton.checked = true;
+}
+const genderRadioButton = document.querySelector(`input[name="gender_update"][value="${product.gender}"]`);
+if (genderRadioButton) {
+    genderRadioButton.checked = true;
+}
+const capacityUpdate = document.querySelector(`input[name="capacity_update"][value="${product.Capacity}"]`);
+if (capacityUpdate) {
+    capacityUpdate.checked = true;
+}
+
+const colorUpdate = document.querySelector(`input[name="color_update"][value="${product.color}"]`);
+if (colorUpdate) {
+    colorUpdate.checked = true;
+}
+document.getElementById("category_update").value = product.category || '';
+
+}
+// hàm thực hiện update, xóa sản phẩm cũ
+function createUpdate(){
+var products = JSON.parse(localStorage.getItem("cardList"));
+const updateProduct = {
+    id: idUpdate,
+    name: document.getElementById("name_update").value,
+    content: document.getElementById("title_update").value,
+    price: parseFloat(document.getElementById("price_update").value),
+    description: document.getElementById("comments_update").value,
+    image:document.getElementById('image').value,
+    tp: document.querySelector('input[name="product_update"]:checked').value,
+    gender: document.querySelector('input[name="gender_update"]:checked').value,
+    capacity: document.querySelector('input[name="capacity_update"]:checked').value,
+    color: document.querySelector('input[name="color_update"]:checked').value,
+    category: document.getElementById("category_update").value
+}
+var replace = products.filter(function(re_product){
+    return String(re_product.id) !== String(idUpdate);
+})
+replace.push(updateProduct);
+localStorage.setItem("cardList", JSON.stringify(replace));
+
+location.reload();
+document.getElementById('update_pro').style.display = 'none';
+console.log("Product updated:", updatedProducts);
+
+
+}
+
+// var productss = JSON.parse(localStorage.getItem("products"));
+// console.log("Store: ", productss)
+function openUser(){
+window.location.href = '../../pages/userAdmin.html';
+}
+function openConfirmation(){
+window.location.href = '../../pages/orderAdmin.html';
+}
+function openChart(){
+window.location.href = '../../pages/chart.html';
+}
+// Phan tim kiem
+var product = JSON.parse(localStorage.getItem("cardList"));
+const searchInput = document.getElementById('header-input');
+const results = document.getElementById('results');
+    // Thêm sự kiện click cho toàn bộ document
+document.addEventListener('click', function(event) {
+    // Kiểm tra nếu click không nằm trong ul và ô input
+if (!results.contains(event.target) && event.target.id !== 'header-input') {
+    results.style.display = 'none';
+}
+});
+searchInput.addEventListener('input', function() {
+const query = this.value.toLowerCase();
+results.innerHTML = ''; // Xóa kết quả trước đó
+results.style.display = 'block';
+const filteredData = product.filter(item => item.name.toLowerCase().includes(query));
+console.log(filteredData)
+filteredData.forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = item.name;
+    li.addEventListener('click', function() {
+        searchInput.value = item.name; // Đặt giá trị ô input thành tên sản phẩm
+        results.innerHTML = ''; // Xóa kết quả sau khi chọn
+    });
+    results.appendChild(li);
+});
+});
+function searchProduct(){
+var dataInput  = document.getElementById('header-input').value;
+var result = product.filter(function(item){
+    return item.name == dataInput;
+});
+
+if(!result){
+    alert("Not found");
+}else{
+    showFind(result);
+}
+}
+function showFind(arr) {
+if (arr && arr.length > 0) {
+    var tableContent = '';  
+
+    for (var i of arr) {
+        var row = "<tr  style = 'border-bottom: 1px solid black; background-color: #faeef0;'>";
+        row += "<td style='padding-left: 90px;font-family: 'Kaisei Tokumin';'>" + i.id + "</td>";
+        row += "<td style='padding-left: 50px;font-family: 'Kaisei Tokumin';'>" + i.name + "</td>";
+        row += "<td style='padding-left: 60px;font-family: 'Kaisei Tokumin';'><img src='" + i.image + "' alt='Product Image' width='100' height='100;'></td>";
+        row += "<td style='padding-left: 90px;'> $" + i.price + "</td>";
+        row += "<td style='padding-left: 70px; font-family: Kaisei Tokumin; '>" +
+        "<button id = 'btn-edit' class='btn btn-success' onclick='updateData(\"" + i.id + "\")'>Edit</button> " +
+        "<button style='font-family: Kaisei Tokumin;' id = 'btn-delete' class='btn btn-danger' onclick='openFormRemove(\"" + i.id + "\")'>Delete</button></td>";
+        row += "</tr>";
+        tableContent += row; 
+    }
+    document.getElementById('content_table').innerHTML = tableContent;
+} else {
+    console.log("No products found in localStorage.");
+}
+}
+
+
+
+
+
+                    
+                    
+
+
